e30cb1cf08236e8b6847779bc20b34383af786b3
iandknoll <knoll075@umn.edu>
2026-01-06 10:37:20 -0600

    first draft of custom button class

--- main.cpp
+++ main.cpp
@@ -53,13 +53,14 @@ class MainButton : public finalcut::FButton
 			//Finally, we have the method inherit from FButton--
 			//Specifically, an instance of FButton w/ pointer "parent" as input
 
-			//Past that, we don't need any code in this block (at least here)
-			//Since we're inheriting constructor behaviour from FButton
 
 			//Side Note: A "conversion function" converts one type to another.
 			//They're declared w/ "operator", or "explicit" for above effect.
 			//Any constructor with one input is a conversion function--
 			//Since it shows how to convert that input type to our new class.
+
+			//Setup callbacks (using user-defined function further down)
+			initCallbacks();
 		}
 
 	private:
@@ -68,22 +69,51 @@ class MainButton : public finalcut::FButton
 		void initLayout()
 		{
 			//Defines a function for setup variables
+			//(Here FPoint is relative to parent dialog) (x,y w,h)
+			setText ("Start Video");
+			setGeometry(finalcut:FPoint{20,7}, finalcut::FSize{20,1});
+		}
+
+		void initCallbacks()
+		{
+			//Defines a function for setting up callbacks
+			addCallback
+			(
+				"clicked",			//Callback Signal
+				&MainButton::cb_cambutton	//Function Pointer
+				std::ref(*this)			//First function arg
+				std::ref(parent)		//Second function arg
+			);
+			//Side-note: std::ref and std:cref (unused) are helper functions--
+			//They generate objects of type std::reference wrapper.
+			//As implied, std::reference_wrapper wraps references (via a pointer?)--
+			//Allowing them to be treated like objects. (Specifically, being copyable)
+			//In turn, a reference is a alternate name for an existing object/variable--
+			//It's sort of like a pointer (an alternate way of accessing the same object)--
+			//But differs in that it's still the same type as said underlying object.
+			//Modifying a reference modifies the underlying variable, and v.versa(?)
+			//std::cref holds a const reference (underlying object can't be modified via it)--
+			//While std:ref holds a reference that can modifying the underlying object
+
+			//(At the moment, I don't fully get the need for a reference wrapper vs. pointer--
+			//Nor reference vs. variable. Other than some upcoming functions expecting them--
+			//But then I'm new to C++  ¯\_(ツ)_/¯ )
+		}
+
+		void cb_cambutton (finalcut::FButton& but, finalcut::FDialog& dgl)
+		{
+			//Function for handling what happens when we press our button
+			//Takes as input a reference to an object of type "finalcut::FButton"
+			//And a reference to an object of type "finalcut::FDialog"
+			//(more on what references are later)
+
+			//Change text of referenced button
+			but.setText("&Stop Video");
+
+			//Redraw referenced dialog
+			dgl.redraw();
 		}
 }
-
-void cb_cambutton (finalcut::FButton& but, finalcut::FDialog& dgl)
-{
-	//Function for handling what happens when we press our button
-	//Takes as input a reference to an object of type "finalcut::FButton"
-	//And a reference to an object of type "finalcut::FDialog"
-	//(more on what references are later)
-
-	//Change text of referenced button
-	but.setText("&Stop Video");
-
-	//Redraw referenced dialog
-	dgl.redraw();
-} 
 
 auto main (int argc, char* argv[]) -> int
 {
@@ -132,21 +162,6 @@ auto main (int argc, char* argv[]) -> int
 		std::ref(button),	//First function argument
 		std::ref(dialog)	//Second function argument
 	);
-	//Side-note: std::ref and std:cref (unused) are helper functions--
-	//They generate objects of type std::reference wrapper.
-	//As implied, std::reference_wrapper wraps references (via a pointer?)--
-	//Allowing them to be treated like objects. (Specifically, being copyable)
-	//In turn, a reference is a alternate name for an existing object/variable--
-	//It's sort of like a pointer (an alternate way of accessing the same object)--
-	//But differs in that it's still the same type as said underlying object.
-	//Modifying a reference modifies the underlying variable, and v.versa(?)
-	//std::cref holds a const reference (underlying object can't be modified via it)--
-	//While std:ref holds a reference that can modifying the underlying object
-
-	//(At the moment, I don't fully get the need for a reference wrapper vs. pointer--
-	//Nor reference vs. variable. Other than some upcoming functions expecting them--
-	//But then I'm new to C++  ¯\_(ツ)_/¯ )
-
 
 	//Sets "dialog" as the main widget for the application.
 	finalcut::FWidget::setMainWidget(&dialog);
